a4e0271de37d3c9f5d7d068422fbd12e8328f250
Benedikt Heine <bebe@bebehei.de>
2018-11-27 13:47:37 +0100

    Testsuite disables bad musl+valgrind tests now by itself

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -15,19 +15,6 @@ cp -r "${DIR_REPO}" "${DIR_BUILD}"
 export PREFIX="${DIR_INST}"
 export CFLAGS='-Werror'
 
-# disable the double tests on Alpine, when running under valgrind
-# Alpine is running with Musl libc and musl uses extended precision
-# doubles, while valgrind can't handle extended precision
-# so 2.3 == atof("2.3") won't be true under valgrind
-#
-# This only matches valgrind, because valgrind's test call is indented
-# with two tabs. (While the normal test call is indented with one tab.)
-if [ "${HOSTNAME}" == "alpine" ]; then
-	sed -i \
-		's/^\t\t.\/test\/test/& -x get_double/' \
-		"${DIR_BUILD}/Makefile"
-fi
-
 make -C "${DIR_BUILD}" clean
 make -C "${DIR_BUILD}" all dunstify test-valgrind
 make -C "${DIR_BUILD}" install
